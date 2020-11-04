ee5c43773f28442f4d4df77952f8cbfb06a200c0
parcorka <65218841+parcorka@users.noreply.github.com>
2020-11-04 14:01:14 +0300

    Add files via upload

---
+++ Минизадача_1/Минизадача_1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Минизадача_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выборФигурыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.кругToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.квадратToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.треуголькникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выборФигурыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выборФигурыToolStripMenuItem
+            // 
+            this.выборФигурыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.кругToolStripMenuItem,
+            this.квадратToolStripMenuItem,
+            this.треуголькникToolStripMenuItem});
+            this.выборФигурыToolStripMenuItem.Name = "выборФигурыToolStripMenuItem";
+            this.выборФигурыToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.выборФигурыToolStripMenuItem.Text = "выбор фигуры";
+            // 
+            // кругToolStripMenuItem
+            // 
+            this.кругToolStripMenuItem.Checked = true;
+            this.кругToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.кругToolStripMenuItem.Name = "кругToolStripMenuItem";
+            this.кругToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.кругToolStripMenuItem.Text = "круг";
+            this.кругToolStripMenuItem.Click += new System.EventHandler(this.кругToolStripMenuItem_Click);
+            // 
+            // квадратToolStripMenuItem
+            // 
+            this.квадратToolStripMenuItem.Name = "квадратToolStripMenuItem";
+            this.квадратToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.квадратToolStripMenuItem.Text = "квадрат";
+            this.квадратToolStripMenuItem.Click += new System.EventHandler(this.квадратToolStripMenuItem_Click);
+            // 
+            // треуголькникToolStripMenuItem
+            // 
+            this.треуголькникToolStripMenuItem.Name = "треуголькникToolStripMenuItem";
+            this.треуголькникToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.треуголькникToolStripMenuItem.Text = "треуголькник";
+            this.треуголькникToolStripMenuItem.Click += new System.EventHandler(this.треуголькникToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Задачка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выборФигурыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem кругToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem квадратToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem треуголькникToolStripMenuItem;
+    }
+}
+
---
+++ Минизадача_1/Минизадача_1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Минизадача_1
+{
+    public partial class Form1 : Form
+    {
+        int checkShape = 0;//фигура
+        bool checkDrag = false;//проверка удержания кнопки мыши
+        bool Exist = false;//существование вершины
+        MyFigure a = new Circle(MousePosition.X, MousePosition.Y);
+        int delX = 0;
+        int delY = 0;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if(Exist) a.Draw(g);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void кругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            квадратToolStripMenuItem.Checked = false;
+            треуголькникToolStripMenuItem.Checked = false;
+            кругToolStripMenuItem.Checked = true;
+            checkShape = 0;
+        }
+
+        private void квадратToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            квадратToolStripMenuItem.Checked = true;
+            треуголькникToolStripMenuItem.Checked = false;
+            кругToolStripMenuItem.Checked = false;
+            checkShape = 1;
+        }
+
+        private void треуголькникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            квадратToolStripMenuItem.Checked = false;
+            треуголькникToolStripMenuItem.Checked = true;
+            кругToolStripMenuItem.Checked = false;
+            checkShape = 2;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) 
+            {
+                if (a.X - 50 <= MousePosition.X && a.X + 50 >= MousePosition.X && a.Y - 50 <= MousePosition.Y && a.Y + 50 >= MousePosition.Y) 
+                {
+                    delX = a.X - MousePosition.X;
+                    delY = a.Y - MousePosition.Y;
+                    checkDrag = true;
+                }
+                else
+                {
+                    Exist = true;
+                    if (checkShape == 0) a = new Circle(MousePosition.X, MousePosition.Y);
+                    if (checkShape == 1) a = new Sqare(MousePosition.X, MousePosition.Y);
+                    if (checkShape == 2) a = new Triangle(MousePosition.X, MousePosition.Y);
+                    Invalidate();
+                }
+            }
+            if (MouseButtons.Right ==  e.Button && a.X - 50 <= MousePosition.X && a.X + 50 >= MousePosition.X && a.Y - 50 <= MousePosition.Y && a.Y + 50 >= MousePosition.Y)
+            {
+                Exist = false;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            checkDrag = false;
+            Invalidate();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (checkDrag)
+            {
+                if (checkShape == 0) a = new Circle(MousePosition.X + delX, MousePosition.Y + delY);
+                if (checkShape == 1) a = new Sqare(MousePosition.X + delX, MousePosition.Y + delY);
+                if (checkShape == 2) a = new Triangle(MousePosition.X + delX, MousePosition.Y + delY);
+                Refresh();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;//изображение не мигает
+        }
+    }
+
+
+
+    abstract class MyFigure
+    {
+        protected int x;
+        protected int y;
+
+        protected MyFigure() { }
+        protected MyFigure(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public abstract int X { get; }
+        public abstract int Y { get; }
+
+        public abstract void Draw(Graphics g);
+    }
+    class Circle : MyFigure
+    {
+        public Circle() : base() { }
+        public Circle(int x, int y) : base(x, y) { }
+        public override int X { get { return x; } }
+        public override int Y { get { return y; } }
+        public override void Draw(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Green), x - 50, y - 50, 100, 100);
+        }
+    }
+    class Sqare : MyFigure
+    {
+        public Sqare() : base() { }
+        public Sqare(int x, int y) : base(x, y) { }
+        public override int X { get { return x; } }
+        public override int Y { get { return y; } }
+        public override void Draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Green), x - 50, y - 50, 100, 100);
+        }
+    }
+    class Triangle : MyFigure
+    {
+        public Triangle() : base() { }
+        public Triangle(int x, int y) : base(x, y) { }
+        public override int X { get { return x; } }
+        public override int Y { get { return y; } }
+        public override void Draw(Graphics g)
+        {
+            Point[] p = { new Point(x, y - 50), new Point(x - 50, y + 50), new Point(x + 50, y + 50) };
+            g.FillPolygon(new SolidBrush(Color.Green), p);
+        }
+    }
+}

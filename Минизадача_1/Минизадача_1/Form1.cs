5f8e800eac0abe9210a19026cdf65ca8c765d4d4
parcorka <65218841+parcorka@users.noreply.github.com>
2020-12-26 14:03:22 +0300

    Add files via upload

--- Минизадача_1/Минизадача_1/Form1.cs
+++ Минизадача_1/Минизадача_1/Form1.cs
@@ -12,22 +12,58 @@ namespace Минизадача_1
 {
     public partial class Form1 : Form
     {
-        int checkShape = 0;//фигура
-        bool checkDrag = false;//проверка удержания кнопки мыши
-        bool Exist = false;//существование вершины
-        MyFigure a = new Circle(MousePosition.X, MousePosition.Y);
-        int delX = 0;
-        int delY = 0;
+        bool ClickCheck;
+        bool InFigureCheck;
+        List<MyFigure> a = new List<MyFigure>();
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            if(Exist) a.Draw(g);
+            foreach (MyFigure f in a)
+            {
+                f.Draw(g);
+            }
+            if(a.Count >= 3)//Минизадача 4
+            {
+                for (int i = 0; i < a.Count; i++)
+                    a[i].Inside = false;
+                for (int i = 0; i < a.Count - 1; i++)
+                {
+                    for(int j = i + 1; j < a.Count; j++)
+                    {
+                        bool above = true;
+                        bool below =true;
+                        for(int k = 0; k < a.Count; k++)
+                        {
+                            if(k != i && k != i)
+                            {
+                                if ((a[k].X - a[i].X) * (a[j].Y - a[i].Y) < (a[k].Y - a[i].Y) * (a[j].X - a[i].X)) below = false;//формулу нашел в интернете)
+                                if ((a[k].X - a[i].X) * (a[j].Y - a[i].Y) > (a[k].Y - a[i].Y) * (a[j].X - a[i].X)) above = false;//и без понятия как она работает)
+                            }
+                        }
+                        if(above || below)
+                        {
+                            a[i].Inside = true;
+                            a[j].Inside = true;
+                            e.Graphics.DrawLine(new Pen(Color.Black), a[i].X, a[i].Y, a[j].X, a[j].Y);
+                        }
+                    }
+                }
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (!a[i].Inside) 
+                    { 
+                        a.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
         }
 
         public Form1()
         {
             InitializeComponent();
+            ClickCheck = false;
         }
 
         private void кругToolStripMenuItem_Click(object sender, EventArgs e)
@@ -35,7 +71,6 @@ namespace Минизадача_1
             квадратToolStripMenuItem.Checked = false;
             треуголькникToolStripMenuItem.Checked = false;
             кругToolStripMenuItem.Checked = true;
-            checkShape = 0;
         }
 
         private void квадратToolStripMenuItem_Click(object sender, EventArgs e)
@@ -43,7 +78,6 @@ namespace Минизадача_1
             квадратToolStripMenuItem.Checked = true;
             треуголькникToolStripMenuItem.Checked = false;
             кругToolStripMenuItem.Checked = false;
-            checkShape = 1;
         }
 
         private void треуголькникToolStripMenuItem_Click(object sender, EventArgs e)
@@ -51,107 +85,219 @@ namespace Минизадача_1
             квадратToolStripMenuItem.Checked = false;
             треуголькникToolStripMenuItem.Checked = true;
             кругToolStripMenuItem.Checked = false;
-            checkShape = 2;
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left) 
+            if (e.Button == MouseButtons.Left)
             {
-                if (a.X - 50 <= MousePosition.X && a.X + 50 >= MousePosition.X && a.Y - 50 <= MousePosition.Y && a.Y + 50 >= MousePosition.Y) 
+                for (int i = 0; i < a.Count; i++)
                 {
-                    delX = a.X - MousePosition.X;
-                    delY = a.Y - MousePosition.Y;
-                    checkDrag = true;
+                    if (a[i].Check(e.X, e.Y))
+                    {
+                        a[i].delX = e.X - a[i].X;
+                        a[i].delY = e.Y - a[i].Y;
+                        a[i].CheckDrag = true;
+                        ClickCheck = true;
+                    }
                 }
-                else
+                if (a.Count >= 3) 
                 {
-                    Exist = true;
-                    if (checkShape == 0) a = new Circle(MousePosition.X, MousePosition.Y);
-                    if (checkShape == 1) a = new Sqare(MousePosition.X, MousePosition.Y);
-                    if (checkShape == 2) a = new Triangle(MousePosition.X, MousePosition.Y);
+                    InFigureCheck = true;
+                    for (int s = 0; s < a.Count - 1; s++)
+                    {
+                        for (int j = s + 1; j < a.Count; j++)
+                        {
+                            bool above = true;
+                            bool below = true;
+                            for (int k = 0; k < a.Count; k++)
+                            {
+                                if (k != s && k != s)
+                                {
+                                    if ((a[k].X - a[s].X) * (a[j].Y - a[s].Y) < (a[k].Y - a[s].Y) * (a[j].X - a[s].X)) below = false;
+                                    if ((a[k].X - a[s].X) * (a[j].Y - a[s].Y) > (a[k].Y - a[s].Y) * (a[j].X - a[s].X)) above = false;
+                                }
+                            }
+                            if (above)
+                            {
+                                if ((e.X - a[s].X) * (a[j].Y - a[s].Y) > (e.Y - a[s].Y) * (a[j].X - a[s].X)) InFigureCheck = false;
+                            }
+                            if (below)
+                            {
+                                if ((e.X - a[s].X) * (a[j].Y - a[s].Y) < (e.Y - a[s].Y) * (a[j].X - a[s].X)) InFigureCheck = false;
+                            }
+                        }
+                    }
+                    if (InFigureCheck)
+                    {
+                        for(int i = 0; i < a.Count; i++)
+                        {
+                            a[i].delX = e.X - a[i].X;
+                            a[i].delY = e.Y - a[i].Y;
+                            a[i].CheckDrag = true;
+                        }
+                    }
+                }
+                if (!ClickCheck && !InFigureCheck)//не нажали ни на один из существующих объектов
+                {
+                    if (кругToolStripMenuItem.Checked) a.Add(new Circle(e.X, e.Y));
+                    if (квадратToolStripMenuItem.Checked) a.Add(new Sqare(e.X, e.Y));
+                    if (треуголькникToolStripMenuItem.Checked) a.Add(new Triangle(e.X, e.Y));
+                    if(a.Count > 2) ReCheck();
                     Invalidate();
                 }
             }
-            if (MouseButtons.Right ==  e.Button && a.X - 50 <= MousePosition.X && a.X + 50 >= MousePosition.X && a.Y - 50 <= MousePosition.Y && a.Y + 50 >= MousePosition.Y)
+            ClickCheck = false;
+            if (MouseButtons.Right == e.Button)
             {
-                Exist = false;
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (a[i].Check(e.X, e.Y)) 
+                    { 
+                        a.RemoveAt(i);
+                        i--;
+                    }
+                }
             }
         }
 
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-            checkDrag = false;
+            for(int i = 0; i< a.Count; i++) a[i].CheckDrag = false;
+            if(a.Count > 2) ReCheck();
             Invalidate();
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (checkDrag)
+            for (int i = 0; i < a.Count; i++)
             {
-                if (checkShape == 0) a = new Circle(MousePosition.X + delX, MousePosition.Y + delY);
-                if (checkShape == 1) a = new Sqare(MousePosition.X + delX, MousePosition.Y + delY);
-                if (checkShape == 2) a = new Triangle(MousePosition.X + delX, MousePosition.Y + delY);
-                Refresh();
+                if (a[i].CheckDrag)
+                {
+                    a[i].X = e.X - a[i].delX;
+                    a[i].Y = e.Y - a[i].delY;
+                }
             }
+            Refresh();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             DoubleBuffered = true;//изображение не мигает
         }
+
+        private void ReCheck()
+        {
+            for (int i = 0; i < a.Count; i++)
+                a[i].Inside = false;
+            for (int i = 0; i < a.Count - 1; i++)
+            {
+                for (int j = i + 1; j < a.Count; j++)
+                {
+                    bool above = true;
+                    bool below = true;
+                    for (int k = 0; k < a.Count; k++)
+                    {
+                        if (k != i && k != i)
+                        {
+                            if ((a[k].X - a[i].X) * (a[j].Y - a[i].Y) < (a[k].Y - a[i].Y) * (a[j].X - a[i].X)) below = false;
+                            if ((a[k].X - a[i].X) * (a[j].Y - a[i].Y) > (a[k].Y - a[i].Y) * (a[j].X - a[i].X)) above = false;
+                        }
+                    }
+                    if (above || below)
+                    {
+                        a[i].Inside = true;
+                        a[j].Inside = true;
+                    }
+                }
+            }
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (!a[i].Inside)
+                {
+                    a.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
     }
-
-
 
     abstract class MyFigure
     {
         protected int x;
         protected int y;
+        protected int R;// радиус
+        protected Color C;
+
+        protected int delx;
+        protected int dely;
+        protected bool checkDrag;
+
+        protected bool inside;
 
         protected MyFigure() { }
         protected MyFigure(int x, int y)
         {
             this.x = x;
             this.y = y;
+            R = 50;//можно менять
+            checkDrag = false;
+            inside = false;
         }
-        public abstract int X { get; }
-        public abstract int Y { get; }
+
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+
+        public int delX { get { return delx; } set { delx = value; } }//?
+        public int delY { get { return dely; } set { dely = value; } }//?
+        public bool CheckDrag { get { return checkDrag; } set { checkDrag = value; } }
+
+        public bool Inside { get { return inside; } set { inside = value; } }
 
         public abstract void Draw(Graphics g);
+        public abstract bool Check(int x, int y);
     }
-    class Circle : MyFigure
-    {
-        public Circle() : base() { }
-        public Circle(int x, int y) : base(x, y) { }
-        public override int X { get { return x; } }
-        public override int Y { get { return y; } }
-        public override void Draw(Graphics g)
+        class Circle : MyFigure
         {
-            g.FillEllipse(new SolidBrush(Color.Green), x - 50, y - 50, 100, 100);
+            public Circle() : base() { }
+            public Circle(int x, int y) : base(x, y) { }
+            public override void Draw(Graphics g)
+            {
+                g.FillEllipse(new SolidBrush(Color.Green), x - R, y - R, 2*R, 2*R);
+            }
+            public override bool Check(int x, int y)
+            {
+            if (Math.Sqrt(Math.Pow(x - this.x, 2) + Math.Pow(y - this.y, 2)) <= R) return true; 
+            else return false;
+            }
         }
-    }
-    class Sqare : MyFigure
-    {
-        public Sqare() : base() { }
-        public Sqare(int x, int y) : base(x, y) { }
-        public override int X { get { return x; } }
-        public override int Y { get { return y; } }
-        public override void Draw(Graphics g)
+        class Sqare : MyFigure
         {
-            g.FillRectangle(new SolidBrush(Color.Green), x - 50, y - 50, 100, 100);
+            public Sqare() : base() { }
+            public Sqare(int x, int y) : base(x, y) { }
+            public override void Draw(Graphics g)
+            {
+                g.FillRectangle(new SolidBrush(Color.Green), x - R, y - R, 2 * R, 2 * R);
+            }
+            public override bool Check(int x, int y)
+            {
+                if (this.x - R <= x && this.x + R >= x && this.y - R <= y && this.y + R >= y) return true;
+                else return false;
+            }
         }
-    }
-    class Triangle : MyFigure
-    {
-        public Triangle() : base() { }
-        public Triangle(int x, int y) : base(x, y) { }
-        public override int X { get { return x; } }
-        public override int Y { get { return y; } }
-        public override void Draw(Graphics g)
+        class Triangle : MyFigure
         {
-            Point[] p = { new Point(x, y - 50), new Point(x - 50, y + 50), new Point(x + 50, y + 50) };
-            g.FillPolygon(new SolidBrush(Color.Green), p);
+            public Triangle() : base() { }
+            public Triangle(int x, int y) : base(x, y) { }
+            public override void Draw(Graphics g)
+            {
+                Point[] p = { new Point(x, y - R), new Point(x - R, y + R), new Point(x + R, y + R) };
+                g.FillPolygon(new SolidBrush(Color.Green), p);
+            }
+            public override bool Check(int x, int y)
+            {
+                if (this.x - R <= x && this.x + R >= x && this.y - R <= y && this.y + R >= y) return true;
+                else return false;
+            }
         }
-    }
 }

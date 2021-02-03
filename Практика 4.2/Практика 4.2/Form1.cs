db956a29f1aed57d61a6790626641882e1522e4c
parcorka <65218841+parcorka@users.noreply.github.com>
2021-02-03 21:46:11 +0300

    Add files via upload

---
+++ Практика 4.2/Практика 4.2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_4._2
+{
+    public partial class Form1 : Form
+    {
+        string data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string Data1 { get { return textBox1.Text; } }
+        public string Data2 { get { return data; } set { data = value; } }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Owner = this;
+            f.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(data != null) textBox2.Text = data;
+        }
+    }
+}
---
+++ Практика 4.2/Практика 4.2/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_4._2
+{
+    public partial class Form2 : Form
+    {
+        Form1 main;
+        public string Data2 { get { return textBox2.Text; } }
+        public Form2()
+        {
+            InitializeComponent();
+            main = this.Owner as Form1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (main.Data1 != null) textBox1.Text = main.Data1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            main.Data2 = textBox2.Text;
+        }
+    }
+}

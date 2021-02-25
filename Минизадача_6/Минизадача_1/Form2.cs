79c01f0cae00d7b5681a390389391c5aa627489d
parcorka <65218841+parcorka@users.noreply.github.com>
2021-02-25 03:05:41 -0600

    Add files via upload

---
+++ Минизадача_6/Минизадача_1/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Минизадача_1
+{
+    public partial class Form2 : Form
+    {
+        int sec;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public int Sec { get { return sec; } set { sec = value; } }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sec = int.Parse(textBox1.Text);
+            DialogResult = DialogResult.OK;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}

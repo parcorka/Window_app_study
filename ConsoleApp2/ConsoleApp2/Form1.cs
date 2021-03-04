ba2b901c9d26633ca2197534bea86ecd8cbe2f0f
parcorka <65218841+parcorka@users.noreply.github.com>
2021-03-04 03:50:36 -0600

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+
+    public partial class Form1 : Form
+    {
+        Color Br;
+        int Radius;
+        delegate void Change();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Br = Color.Red;
+            Radius = 25;
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillEllipse(new SolidBrush(Br), 50, 50, Radius, Radius);
+        }
+        private void RadDel(object sender, RadiusEventArgs e)
+        {
+            Radius = e.Rad;
+            Refresh();
+        }
+
+        private void изменениеРадиусаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+            f.Show();
+            f.s += RadDel;
+        }
+    }
+    
+}
---
+++ ConsoleApp2/ConsoleApp2/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    public delegate void SizeChange(object sender, RadiusEventArgs e);
+    public partial class Form2 : Form
+    {
+        public event SizeChange s;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int v = Convert.ToInt32(numericUpDown1.Value);
+            if(this.s != null)
+            {
+                s(this, new RadiusEventArgs(v));
+            }
+        }
+    }
+    public class RadiusEventArgs : EventArgs
+    {
+        //private int rad;
+        //public int Rad { get { return rad; } set { Rad = value; } }
+        public int Rad { get; set; }
+        public RadiusEventArgs(int r)
+        {
+            Rad = r;
+        }
+    }
+}
